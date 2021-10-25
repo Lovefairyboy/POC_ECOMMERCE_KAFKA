9acf0a78632952739e9fd97b0e1a6a89bd1cafe9
Lovefairyboy <110706152+Lovefairyboy@users.noreply.github.com>
2021-10-25 11:57:58 +0000

    Fix #66

--- src/index.js
+++ src/index.js
@@ -11,3 +11,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
