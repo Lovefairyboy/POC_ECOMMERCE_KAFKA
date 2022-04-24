d0c7b3f9ea0efeb9e1f38e31986c5b6968c844d9
Lovefairyboy <110706152+Lovefairyboy@users.noreply.github.com>
2022-04-24 14:25:23 +0000

    NFSv4: Fix races between nfs_remove_bad_delegation() and delegation return

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,3 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getF
+    import('web-vitals').then(({ getCLS, getFI
