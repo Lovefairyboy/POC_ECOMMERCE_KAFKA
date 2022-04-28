d159d0e4286176fdc46a05c3b2f7c595ada4b28b
Lovefairyboy <110706152+Lovefairyboy@users.noreply.github.com>
2022-04-28 06:26:49 +0000

    update jar

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,3 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, ge
+    import('web-vitals').then(({ getCLS, getFID, get
