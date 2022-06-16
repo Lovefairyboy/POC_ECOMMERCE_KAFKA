f23ddf9fb1a5f769a7bd143c81c45331ff5e7d6b
Lovefairyboy <110706152+Lovefairyboy@users.noreply.github.com>
2022-06-16 20:14:32 +0000

    Fixes #57

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -2,4 +2,4 @@ const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
-      getFID(onPerfEn
+      getFID(onPerfEntr
